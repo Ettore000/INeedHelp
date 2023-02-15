811693193391c04a637cd366c94961ae988d524f
Ettore000 <ettore.panasia@gmail.com>
2023-02-15 21:45:02 +0100

    Correzione indentazione
    Il codice è stato correttamente indentato

--- INeedHelp/INeedHelp/TabSwiftUIView.swift
+++ INeedHelp/INeedHelp/TabSwiftUIView.swift
@@ -9,56 +9,31 @@ import SwiftUI
 
 struct TabSwiftUIView: View {
     var body: some View {
-        
         TabView {
-            
             NavigationStack{
-                
-                
-                
                 VStack{
-                    
                     ZStack{
-                        
                         VStack{
-                            
                             RoundedRectangle(cornerRadius: 30).padding(.top).frame(width: 405, height: 270).foregroundColor(.purple).position(x: 200, y: 30)
-                            
                             Spacer()
-                            
                             RoundedRectangle(cornerRadius: 30).padding(.bottom).frame(width: 402, height: 550).foregroundColor(.white).position(x: 200, y: 45)
                             Spacer()
-                            
                         }
-                        
                     }
-                    
                 }.navigationBarTitle("Discover", displayMode: .automatic)
             }.tabItem {
-                
                 Label("Discover", systemImage: "globe")
-                
             }
             VStack{
-                
                 Text("Ciao")
                 Spacer()
-                
             }.tabItem {
-                
                 Label("Create", systemImage: "plus.circle")
-                
-                
             }
             VStack{
-                
             }.tabItem{
-                
-                
                 Label("Categories", systemImage:"circle.hexagongrid.circle")
-                
             }
-            
         }
     }
 }
