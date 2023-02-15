9b7d4320ab1c2302d62224037017c08fad5e21ea
Ettore000 <ettore.panasia@gmail.com>
2023-02-15 18:34:24 +0100

    Commit del codice iniziale

---
+++ INeedHelp/INeedHelp/INeedHelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  INeedHelpApp.swift
+//  INeedHelp
+//
+//  Created by ettoreantoniopanasia on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct INeedHelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
