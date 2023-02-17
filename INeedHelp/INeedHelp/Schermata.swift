824176a7581225c3079aa64d6a667ba3eec9a61d
maricap26 <125493657+maricap26@users.noreply.github.com>
2023-02-17 12:52:00 +0100

    nuovo

---
+++ INeedHelp/INeedHelp/Schermata.swift
@@ -0,0 +1,20 @@
+//
+//  Schermata.swift
+//  INeedHelp
+//
+//  Created by maricaparente on 17/02/23.
+//
+
+import SwiftUI
+
+struct Schermata: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Schermata_Previews: PreviewProvider {
+    static var previews: some View {
+        Schermata()
+    }
+}
