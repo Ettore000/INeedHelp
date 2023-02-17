130bb7b1eea3bf8acb0d2bdf7fead367783648a4
barbara98sicuranza <barsic98@gmail.com>
2023-02-17 12:58:02 +0100

    17Feb

---
+++ INeedHelp/INeedHelp/Launch.swift
@@ -0,0 +1,20 @@
+//
+//  Launch.swift
+//  INeedHelp
+//
+//  Created by barbarasicuranza on 17/02/23.
+//
+
+import SwiftUI
+
+struct Launch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Launch_Previews: PreviewProvider {
+    static var previews: some View {
+        Launch()
+    }
+}
