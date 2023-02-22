86a0760bd67871c68a75af85b4c8fb481a5afc9a
Ettore000 <ettore.panasia@gmail.com>
2023-02-22 17:51:26 +0100

    Ettore

--- INeedHelp/INeedHelp/Selections/NewUIView.swift
+++ INeedHelp/INeedHelp/Selections/NewUIView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct NewUIView: View {
+    @State private var searchText = ""
+    @State private var isLinkActive = false
+    
     var body: some View {
         NavigationStack {
             ZStack {
@@ -21,21 +24,69 @@ struct NewUIView: View {
                                 .padding()
                             Spacer()
                         }
-                        HStack {
-                            Text("Barra di ricerca")
-                                .foregroundColor(.white)
-                        }
                         Spacer()
                         RoundedRectangle(cornerRadius: 35)
                             .fill(Color.white)
                             .ignoresSafeArea()
                             .overlay(
-                                Text("Ciao")
+                                //NavigationView {
+                                    VStack {
+                                        HStack {
+                                            Button(action: {
+                                                // Do something to cancel and go back
+                                            }, label: {
+                                                Image(systemName: "xmark")
+                                                    .foregroundColor(.black)
+                                            })
+                                            Spacer()
+                                            NavigationLink(
+                                                destination: NextPage(),
+                                                isActive: $isLinkActive,
+                                                label: {
+                                                    Text("Avanti")
+                                                })
+                                        }
+                                        .padding()
+                                        TextField(searchText, text: $searchText)
+                                            .padding()
+                                        Text("What are you thinking about?")
+                                            .font(.headline)
+                                            .padding()
+                                        HStack {
+                                            Button("Text") {
+                                                // Do something when the Text button is pressed
+                                            }
+                                            Button("Link") {
+                                                // Do something when the Link button is pressed
+                                                isLinkActive = true
+                                            }
+                                            Button("Image") {
+                                                // Do something when the Image button is pressed
+                                            }
+                                            Button("Video") {
+                                                // Do something when the Video button is pressed
+                                            }
+                                            Button("Survey") {
+                                                // Do something when the Survey button is pressed
+                                            }
+                                        }
+                                        .padding()
+                                        Spacer()
+                                    }
+                                    .navigationBarHidden(true)
+                                //}
                             )
                     }
                 }
             }
         }
+    }
+}
+
+struct NextPage: View {
+    var body: some View {
+        // Code for the next page goes here
+        Text("Next Page")
     }
 }
 
--- INeedHelp/INeedHelp/TestUIView.swift
+++ INeedHelp/INeedHelp/TestUIView.swift
@@ -7,54 +7,39 @@
 import SwiftUI
 
 struct TestUIView: View {
-    let authorName: String
-    let authorImageName: String
-    let title: String
-    let bodyText: String
-    let likesCount: Int
-    let timestamp: Date
+    @State private var showDetails = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-                    HStack(spacing: 8) {
-                        Image(authorImageName)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 32, height: 32)
-                            .clipShape(Circle())
-                        Text(authorName)
-                            .font(.subheadline)
-                        Spacer()
-                        Text(timeAgoDisplay())
-                            .font(.caption)
-                    }
-                    Text(title)
-                        .font(.headline)
-                    Text(bodyText)
-                        .font(.body)
-                    HStack {
-                        Image(systemName: "heart.fill")
-                            .foregroundColor(.red)
-                        Text("\(likesCount) Likes")
+        ZStack {
+            GradientBackground()
+            VStack {
+                Button("Mostra dettagli") {
+                    withAnimation {
+                        self.showDetails.toggle()
                     }
                 }
-                .padding(.horizontal)
-    }
-    
-    func timeAgoDisplay() -> String {
-            let formatter = RelativeDateTimeFormatter()
-            formatter.unitsStyle = .abbreviated
-            return formatter.localizedString(for: timestamp, relativeTo: Date())
+                if showDetails {
+                    DetailsView()
+                        .transition(.move(edge: .top))
+                }
+            }
         }
+    }
 }
 
 struct TestUIView_Previews: PreviewProvider {
     static var previews: some View {
-            TestUIView(authorName: "Marco Abbate",
-                     authorImageName: "fp2",
-                     title: "My cat is running everywhere in the house",
-                     bodyText: "I love my cat, but she's always on the move. She runs up and down...",
-                     likesCount: 45,
-                     timestamp: Date().addingTimeInterval(-3600))
+        TestUIView()
+    }
+}
+
+struct DetailsView: View {
+    var body: some View {
+        ZStack {
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            
+            Text("Dettagli della pagina")
         }
+    }
 }
