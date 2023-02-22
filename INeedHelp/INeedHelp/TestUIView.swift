00e01dd22939cac8f4de42ee403614fe80a214cc
Ettore000 <ettore.panasia@gmail.com>
2023-02-22 16:27:22 +0100

    Update TestUIView.swift

--- INeedHelp/INeedHelp/TestUIView.swift
+++ INeedHelp/INeedHelp/TestUIView.swift
@@ -7,42 +7,10 @@
 import SwiftUI
 
 struct TestUIView: View {
-    let likesCount: Int
-    let timeElapsed: TimeInterval
-    
     var body: some View {
-        HStack {
-            Image(systemName: "heart.fill")
-                .foregroundColor(.red)
-            Text("\(likesCount) Mi piace")
-            Spacer()
-            Text(timeString(timeElapsed: timeElapsed))
-        }
-        .padding()
-    }
-    
-    private func timeString(timeElapsed: TimeInterval) -> String {
-        let formatter = DateComponentsFormatter()
-        formatter.unitsStyle = .abbreviated
-        formatter.allowedUnits = [.second, .minute, .hour, .day]
-        return formatter.string(from: timeElapsed)!
+        Text("Prova")
     }
 }
-
-struct prova: View {
-    
-    let post = Post(likesCount: 23, timeElapsed: 3600)
-    
-    var body: some View {
-        TestUIView(likesCount: post.likesCount, timeElapsed: post.timeElapsed)
-    }
-}
-
-struct Post {
-    let likesCount: Int
-    let timeElapsed: TimeInterval
-}
-
 
 struct TestUIView_Previews: PreviewProvider {
     static var previews: some View {
