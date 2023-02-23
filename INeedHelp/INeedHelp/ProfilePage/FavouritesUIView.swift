ba0e30e8d2e4fa0a6b910928e86ecdf1f62e813d
Ettore000 <ettore.panasia@gmail.com>
2023-02-23 11:45:42 +0100

    Ettore

--- INeedHelp/INeedHelp/ProfilePage/FavouritesUIView.swift
+++ INeedHelp/INeedHelp/ProfilePage/FavouritesUIView.swift
@@ -12,73 +12,113 @@ struct FavouritesUIView: View {
         ZStack {
             GradientBackground()
             VStack {
-                
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
-                    .ignoresSafeArea()
-                    .frame(width: 385, height: 150)
-                    .overlay(
-                        VStack {
-                            PostUIView(authorName: "Marco Abbate",
-                                       authorImageName: "fp2",
-                                       title: "My cat is running everywhere in the house",
-                                       bodyText: "I love my cat, but she's always on the move. She runs up and down the stairs, jumps on the furniture, and even plays with her toys at all hours of the day and night. I'm worried that she's going to hurt herself or break something in the house.",
-                                       likesCount: 8,
-                                       timestamp: Date().addingTimeInterval(-360),
-                                       postCat: "Animals"
-                            )
-                        }
-                    )
-                
+                HStack {
+                    Text("Favourites")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                    Spacer()
+                }
+                ScrollView(.vertical) {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
+                        .ignoresSafeArea()
+                        .frame(width: 385, height: 150)
+                        .overlay(
+                            VStack {
+                                PostUIView(authorName: "Marco Abbate",
+                                           authorImageName: "fp2",
+                                           title: "My cat is running everywhere in the house",
+                                           bodyText: "I love my cat, but she's always on the move. She runs up and down the stairs, jumps on the furniture, and even plays with her toys at all hours of the day and night. I'm worried that she's going to hurt herself or break something in the house.",
+                                           likesCount: 8,
+                                           timestamp: Date().addingTimeInterval(-360),
+                                           postCat: "Animals"
+                                )
+                            }
+                        )
                     
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
-                    .ignoresSafeArea()
-                    .frame(width: 385, height: 150)
-                    .overlay(
-                        VStack {
-                            PostUIView(authorName: "Chiara Rizzi",
-                                       authorImageName: "fp3",
-                                       title: "How to cook pizza at home?",
-                                       bodyText: "I love eating pizza on saturday night, it's a tradition...",
-                                       likesCount: 15,
-                                       timestamp: Date().addingTimeInterval(-2400),
-                                       postCat: "Food")
-                        }
-                    )
-                
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
-                    .ignoresSafeArea()
-                    .frame(width: 385, height: 150)
-                    .overlay(
-                        VStack {
-                            PostUIView(authorName: "Sofia Romano",
-                                       authorImageName: "fp3",
-                                       title: "My cat has a broken paw. ",
-                                       bodyText: "How can i relieve him?",
-                                       likesCount: 30,
-                                       timestamp: Date().addingTimeInterval(-3100),
-                                       postCat: "Animals")
-                        }
-                    )
-                
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
-                    .ignoresSafeArea()
-                    .frame(width: 385, height: 150)
-                    .overlay(
-                        VStack {
-                            PostUIView(authorName: "Giorgio Costa",
-                                       authorImageName: "fp5",
-                                       title: "I'm arranging the garden of my new house.",
-                                       bodyText: "Wheni the best time to plant flower beds?",
-                                       likesCount: 50,
-                                       timestamp: Date().addingTimeInterval(-3600),
-                                       postCat: "Gardening")
-                            
-                        }
-                    )
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
+                        .ignoresSafeArea()
+                        .frame(width: 385, height: 150)
+                        .overlay(
+                            VStack {
+                                PostUIView(authorName: "Chiara Rizzi",
+                                           authorImageName: "fp3",
+                                           title: "How to cook pizza at home?",
+                                           bodyText: "I love eating pizza on saturday night, it's a tradition...",
+                                           likesCount: 15,
+                                           timestamp: Date().addingTimeInterval(-2400),
+                                           postCat: "Food")
+                            }
+                        )
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
+                        .ignoresSafeArea()
+                        .frame(width: 385, height: 150)
+                        .overlay(
+                            VStack {
+                                PostUIView(authorName: "Sofia Romano",
+                                           authorImageName: "fp3",
+                                           title: "My cat has a broken paw. ",
+                                           bodyText: "How can i relieve him?",
+                                           likesCount: 30,
+                                           timestamp: Date().addingTimeInterval(-3100),
+                                           postCat: "Animals")
+                            }
+                        )
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
+                        .ignoresSafeArea()
+                        .frame(width: 385, height: 150)
+                        .overlay(
+                            VStack {
+                                PostUIView(authorName: "Giorgio Costa",
+                                           authorImageName: "fp5",
+                                           title: "I'm arranging the garden of my new house.",
+                                           bodyText: "When is the best time to plant flower beds?",
+                                           likesCount: 9,
+                                           timestamp: Date().addingTimeInterval(-1234),
+                                           postCat: "Gardening")
+                                
+                            }
+                        )
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
+                        .ignoresSafeArea()
+                        .frame(width: 385, height: 150)
+                        .overlay(
+                            VStack {
+                                PostUIView(authorName: "Antonio Buonarroti",
+                                           authorImageName: "fp1",
+                                           title: "I don't understand physics 1.",
+                                           bodyText: "I need help understanding Proust's law",
+                                           likesCount: 21,
+                                           timestamp: Date().addingTimeInterval(-643),
+                                           postCat: "School")
+                                
+                            }
+                        )
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)))
+                        .ignoresSafeArea()
+                        .frame(width: 385, height: 150)
+                        .overlay(
+                            VStack {
+                                PostUIView(authorName: "Fabio Norman",
+                                           authorImageName: "fp2",
+                                           title: "My back hurts.",
+                                           bodyText: "I'm looking for advice what read to buy",
+                                           likesCount: 34,
+                                           timestamp: Date().addingTimeInterval(-3456),
+                                           postCat: "Health")
+                                
+                            }
+                        )
+                }
             }
         }
     }
--- INeedHelp/INeedHelp/SignInForms/CongratulationView.swift
+++ INeedHelp/INeedHelp/SignInForms/CongratulationView.swift
@@ -9,51 +9,38 @@ import SwiftUI
 
 struct CongratulationView: View {
     var body: some View {
-        ZStack {
-            GradientBackground()
-            
-            VStack{
-                Image(systemName: "person.crop.circle.badge.checkmark")
-                    .font(.system(size: 200))
-                    .foregroundColor(.white)
-                    .position(x:195, y:250)
-                
-                
-            }
-            
-            
-            VStack {
-                Text("Congratulations!")
-                    .foregroundColor(Color.white)
-                    .font(.largeTitle)
-                    .multilineTextAlignment(.center)
-                    .position(x:195, y:450)
-                   
-                    
-            }
-            
-            
-            VStack{
-                Text("You have successfully completed the registration.")
-                    .foregroundColor(Color.white)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .position(x: 196, y: 550)
-            }
-            
-            VStack{
-                Button(action: { /* azione*/})
-                {
-                    Text("Start")
-                        .font(.largeTitle.bold())
+        NavigationStack {
+            ZStack {
+                GradientBackground()
+                VStack{
+                    Spacer()
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .font(.system(size: 200))
                         .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, minHeight: 44)
+                    Text("Congratulations!")
+                        .foregroundColor(Color.white)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    Text("You have successfully completed the registration.")
+                        .foregroundColor(Color.white)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.orange)
                         .frame(width:150, height:70)
-                        .background(Color.orange)
-//                        .background(Color(red: 0.8705882352941177, green: 0.5411764705882353, blue: 0.4470588235294118))
-                        .cornerRadius(15)
                         .padding(.horizontal)
-                        .position(x:196, y:700)
+                        .overlay(
+                            NavigationLink(destination: TabBarUIView()
+                                .navigationBarBackButtonHidden(true)) {
+                                HStack {
+                                    Text("Start")
+                                        .font(.largeTitle.bold())
+                                        .foregroundColor(.white)
+                                }
+                            }
+                        )
+                    Spacer()
                 }
             }
         }
