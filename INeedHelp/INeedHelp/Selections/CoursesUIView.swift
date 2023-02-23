9fb4140dded866945c1b9265f1ba4f9bc5c83720
Antonioperna <125458184+Antonioperna@users.noreply.github.com>
2023-02-23 10:31:20 +0100

    Antonio

--- INeedHelp/INeedHelp/Selections/CoursesUIView.swift
+++ INeedHelp/INeedHelp/Selections/CoursesUIView.swift
@@ -18,27 +18,24 @@ struct CoursesUIView: View {
                         .font(.title3)
                         .fontWeight(.black)
                         .foregroundColor(Color.white)
-                        //.multilineTextAlignment(.center)
+                    //.multilineTextAlignment(.center)
                         .frame(height: 5.0)
-                    Spacer()
-                    Spacer()
-                    Spacer()
+                        .position(x:55,y:10)
                 }.padding()
-                Spacer()
-                Spacer()
+              
                 Text("Sweets")
                     .font(.headline)
                     .foregroundColor(.black)
                     .frame(maxWidth: .infinity, minHeight: 44)
-                    .frame(width:234, height:60)
+                    .frame(width:234, height:70)
                     .background(Color.white.opacity(0.5).gradient)
                     .cornerRadius(15)
-                    .padding(.horizontal)
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
+                    .padding(.horizontal).position(x:200,y:-60)
+                Image("sweet")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .position(x:80,y:-250)
                 VStack{
                     Text("Italian cuisine")
                         .font(.headline)
@@ -47,11 +44,12 @@ struct CoursesUIView: View {
                         .frame(width:234, height:70)
                         .background(Color.white.opacity(0.5).gradient)
                         .cornerRadius(15)
-                        .padding(.horizontal)
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Spacer()
+                        .padding(.horizontal).position(x:200,y:-280)
+                    Image("sweet")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .position(x:80,y:-250)
                     VStack{
                         Text("Fast food")
                             .font(.headline)
@@ -59,14 +57,8 @@ struct CoursesUIView: View {
                             .frame(maxWidth: .infinity, minHeight: 44)
                             .frame(width:234, height:70)
                             .background(Color.white.opacity(0.5).gradient)
-
                             .cornerRadius(15)
-                            .padding(.horizontal)
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
-                        Spacer()
+                            .padding(.horizontal).position(x:200,y:-295)
                         VStack{
                             Text("Fruit")
                                 .font(.headline)
@@ -74,14 +66,8 @@ struct CoursesUIView: View {
                                 .frame(maxWidth: .infinity, minHeight: 44)
                                 .frame(width:234, height:70)
                                 .background(Color.white.opacity(0.5).gradient)
-
                                 .cornerRadius(15)
-                                .padding(.horizontal)
-                            Spacer()
-                            Spacer()
-                            Spacer()
-                            Spacer()
-                            Spacer()
+                                .padding(.horizontal).position(x:200,y:-250)
                             VStack{
                                 Text("Vegetables")
                                     .font(.headline)
@@ -89,14 +75,8 @@ struct CoursesUIView: View {
                                     .frame(maxWidth: .infinity, minHeight: 44)
                                     .frame(width:234, height:70)
                                     .background(Color.white.opacity(0.5).gradient)
-
                                     .cornerRadius(15)
-                                    .padding(.horizontal)
-                                Spacer()
-                                Spacer()
-                                Spacer()
-                                Spacer()
-                                Spacer()
+                                    .padding(.horizontal).position(x:200,y:-180)
                                 VStack{
                                     Text("Oriental cuisine")
                                         .font(.headline)
@@ -104,30 +84,47 @@ struct CoursesUIView: View {
                                         .frame(maxWidth: .infinity, minHeight: 44)
                                         .frame(width:234, height:70)
                                         .background(Color.white.opacity(0.5).gradient)
-
                                         .cornerRadius(15)
-                                        .padding(.horizontal)
-                                    Spacer()
-                                    Spacer()
-                                    Spacer()
-                                    Spacer()
-                                    Spacer()
+                                        .padding(.horizontal).position(x:200,y:-90)
                                 }
                             }
                         }
                     }
                     
                 }
-                
-                    
-                
+                /*Image("sweet")
+                    .overlay(
+                        VStack {
+                            Text("Sweets")
+                                .font(.headline)
+                                .foregroundColor(.black)
+                                .padding(.horizontal)
+                        }
+                    )*/
             }
-        
             
         }
     }
         }
         
+/*struct prova: View {
+    var body: some View {
+        image("sweet")
+            .overlay(
+                VStack {
+                    Text("Sweets")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, minHeight: 44)
+                        .frame(width:234, height:70)
+                        .background(Color.white.opacity(0.5).gradient)
+                        .cornerRadius(15)
+                        .padding(.horizontal).position(x:200,y:-130)
+                }
+            )
+        }
+    }*/
+
 
 struct CoursesUIView_Previews: PreviewProvider {
     static var previews: some View {
